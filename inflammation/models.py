65737dd02f141ed9714fb393626801aa6fe4a0e6
JFriske <jennifer.friske.20@ucl.ac.uk>
2025-11-10 15:19:06 +0100

    Renamed "max" to "max_data" to avoid unwanted overwriting of max() Also includes formatting changes from ruff

--- inflammation/models.py
+++ inflammation/models.py
@@ -42,15 +42,15 @@ def patient_normalise(data):
     :param data: 2D array of inflammation data
     :return: normalised 2D array of inflammation data
     """
-    
+
     if not isinstance(data, np.ndarray):
         raise TypeError('data input should be ndarray')
     if len(data.shape) != 2:
         raise ValueError('inflammation array should be 2-dimensional')
     if np.any(data < 0):
         raise ValueError('inflammation values should be non-negative')
-    max = np.nanmax(data, axis=1)
+    max_data = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
-        normalised = data / max[:, np.newaxis]
+        normalised = data / max_data[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
     return normalised
