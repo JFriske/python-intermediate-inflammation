5423000df6b160e89d5059920d93422c58b93194
JFriske <jennifer.friske.20@ucl.ac.uk>
2025-11-11 12:34:34 +0100

    Added support for JSON as input files

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -7,6 +7,28 @@ import numpy as np
 
 from inflammation import models, views
 
+class JSONDataSource:
+    """ Data class used to read inflammation data from JSON files in a directory. 
+    :param data_dir: Directory containing inflammation JSON files.
+    """
+    
+    def __init__(self, data_dir: str):
+        self.data_dir = data_dir
+    
+    def load_inflammation_data(self, file_identifier: str = 'inflammation*.json') -> list[np.ndarray]:
+        """Loads all inflammation data JSON files from a directory.
+
+        :param data_dir: Directory containing inflammation JSON files.
+        :return: List of Numpy arrays with inflammation data.
+        """
+        data_file_paths = glob.glob(os.path.join(self.data_dir, file_identifier))
+        if len(data_file_paths) == 0:
+            raise ValueError(f"No inflammation data JSON files found in path {self.data_dir}")
+        data = map(models.load_json, data_file_paths)
+        flattened_data = []
+        for dataset in data:
+            flattened_data.extend(dataset)
+        return flattened_data
 
 class CSVDataSource:
     """ Data class used to read inflammation data from CSV files in a directory. 
@@ -29,7 +51,7 @@ class CSVDataSource:
         return list(data) 
 
 
-def analyse_data(data_source: CSVDataSource):
+def analyse_data(data_source: CSVDataSource|JSONDataSource):
     """Calculates the standard deviation by day between datasets.
 
     Gets all the inflammation data from CSV files within a directory,
